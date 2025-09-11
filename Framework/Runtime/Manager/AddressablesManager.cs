09b5b64fbaa474a9007814c830651e34d7dd9204
xiehao <1286384820@qq.com>
2025-09-11 09:39:52 +0800

    添加Assembly Definition

--- Framework/Runtime/Manager/AddressablesManager.cs
+++ Framework/Runtime/Manager/AddressablesManager.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
-using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.U2D;
 
 /// <summary>
