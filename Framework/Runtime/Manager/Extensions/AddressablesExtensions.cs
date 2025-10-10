e3e94767a7060fc339660a67685f928a22d6c181
hao xie <1286384820@qq.com>
2025-10-10 17:10:37 +0800

    文件分类

---
+++ Framework/Runtime/Manager/Extensions/AddressablesExtensions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace FDIM.Framework
+{
+    /// <summary>
+    /// 扩展方法工具类
+    /// </summary>
+    public static class AddressablesExtensions
+    {
+        /// <summary>
+        /// 通过Id直接返回路径
+        /// </summary>
+        public static T GetByPath<T>(this int id)
+        {
+            return Managers.ConfigManager.GetById<T>(id);
+        }
+
+    }
+}
