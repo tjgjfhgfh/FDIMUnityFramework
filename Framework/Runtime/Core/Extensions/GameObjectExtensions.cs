6d12fd0157d7e86ecee05891b8307ae720e4c59b
hao xie <1286384820@qq.com>
2025-10-11 14:55:12 +0800

    扩展一下链式函数

--- Framework/Runtime/Core/Extensions/GameObjectExtensions.cs
+++ Framework/Runtime/Core/Extensions/GameObjectExtensions.cs
@@ -8,15 +8,58 @@ public static class GameObjectExtensions
         return go;
     }
 
-    public static GameObject SetName(this GameObject go, string name)
+    public static GameObject SetNameEx(this GameObject go, string name)
     {
         go.name = name;
         return go;
     }
 
-    public static GameObject SetParentEx(this GameObject go, Transform parent)
+    public static GameObject SetParentEx(this GameObject go, Transform parent, bool worldPositionStays = true)
     {
-        go.transform.SetParent(parent);
+        go.transform.SetParent(parent, worldPositionStays);
         return go;
     }
+
+    public static GameObject SetLayerEx(this GameObject go, int layer)
+    {
+        go.layer = layer;
+        return go;
+    }
+
+    public static GameObject SetTagEx(this GameObject go, string tag)
+    {
+        go.tag = tag;
+        return go;
+    }
+
+    public static GameObject SetPositionEx(this GameObject go, Vector3 pos)
+    {
+        go.transform.position = pos;
+        return go;
+    }
+
+    public static GameObject SetLocalPositionEx(this GameObject go, Vector3 pos)
+    {
+        go.transform.localPosition = pos;
+        return go;
+    }
+
+    public static GameObject SetRotationEx(this GameObject go, Quaternion rot)
+    {
+        go.transform.rotation = rot;
+        return go;
+    }
+
+    public static GameObject SetLocalRotationEx(this GameObject go, Quaternion rot)
+    {
+        go.transform.localRotation = rot;
+        return go;
+    }
+
+    public static GameObject SetScaleEx(this GameObject go, Vector3 scale)
+    {
+        go.transform.localScale = scale;
+        return go;
+    }
+
 }
--- Framework/Runtime/Core/Extensions/TransformExtensions.cs
+++ Framework/Runtime/Core/Extensions/TransformExtensions.cs
@@ -2,39 +2,63 @@ using UnityEngine;
 
 public static class TransformExtensions
 {
-    public static Transform SetPosition(this Transform t, Vector3 pos)
+    public static Transform SetActiveEx(this Transform t, bool active)
+    {
+        t.gameObject.SetActive(active);
+        return t;
+    }
+
+    public static Transform SetNameEx(this Transform t, string name)
+    {
+        t.gameObject.name = name;
+        return t;
+    }
+
+    public static Transform SetParentEx(this Transform t, Transform parent, bool worldPositionStays = true)
+    {
+        t.SetParent(parent, worldPositionStays);
+        return t;
+    }
+
+    public static Transform SetLayerEx(this Transform t, int layer)
+    {
+        t.gameObject.layer = layer;
+        return t;
+    }
+
+    public static Transform SetTagEx(this Transform t, string tag)
+    {
+        t.gameObject.tag = tag;
+        return t;
+    }
+
+    public static Transform SetPositionEx(this Transform t, Vector3 pos)
     {
         t.position = pos;
         return t;
     }
 
-    public static Transform SetLocalPosition(this Transform t, Vector3 pos)
+    public static Transform SetLocalPositionEx(this Transform t, Vector3 pos)
     {
         t.localPosition = pos;
         return t;
     }
 
-    public static Transform SetRotation(this Transform t, Quaternion rot)
+    public static Transform SetRotationEx(this Transform t, Quaternion rot)
     {
         t.rotation = rot;
         return t;
     }
 
-    public static Transform SetLocalRotation(this Transform t, Quaternion rot)
+    public static Transform SetLocalRotationEx(this Transform t, Quaternion rot)
     {
         t.localRotation = rot;
         return t;
     }
 
-    public static Transform SetScale(this Transform t, Vector3 scale)
+    public static Transform SetScaleEx(this Transform t, Vector3 scale)
     {
         t.localScale = scale;
-        return t;
-    }
-
-    public static Transform SetParentEx(this Transform t, Transform parent)
-    {
-        t.SetParent(parent);
         return t;
     }
 }
